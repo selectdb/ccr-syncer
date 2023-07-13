ae94a723c6bf81f50375f5e51aa8805f79edc0d3
Jack Drogon <jack.xsuperman@gmail.com>
2023-07-13 15:57:01 +0800

    Update erros.Wrapf for sqlite3.go

--- storage/sqlite.go
+++ storage/sqlite.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
 )
 
 type SQLiteDB struct {
@@ -13,17 +14,17 @@ type SQLiteDB struct {
 func NewSQLiteDB(dbPath string) (DB, error) {
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, "open sqlite3 path %s failed", dbPath)
 	}
+
 	// create table info && progress, if not exists
 	// all is tuple (string, string)
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS jobs (job_name TEXT PRIMARY KEY, job_info TEXT)")
-	if err != nil {
-		return nil, err
+	if _, err = db.Exec("CREATE TABLE IF NOT EXISTS jobs (job_name TEXT PRIMARY KEY, job_info TEXT)"); err != nil {
+		return nil, errors.Wrapf(err, "create table jobs failed")
 	}
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS progresses (job_name TEXT PRIMARY KEY, progress TEXT)")
-	if err != nil {
-		return nil, err
+
+	if _, err = db.Exec("CREATE TABLE IF NOT EXISTS progresses (job_name TEXT PRIMARY KEY, progress TEXT)"); err != nil {
+		return nil, errors.Wrapf(err, "create table progresses failed")
 	}
 
 	return &SQLiteDB{db: db}, nil
@@ -32,49 +33,55 @@ func NewSQLiteDB(dbPath string) (DB, error) {
 func (s *SQLiteDB) AddJob(jobName string, jobInfo string) error {
 	// check job name exists, if exists, return error
 	var count int
-	err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count)
-	if err != nil {
-		return err
+	if err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count); err != nil {
+		return errors.Wrapf(err, "query job name %s failed", jobName)
 	}
+
 	if count > 0 {
 		return ErrJobExists
 	}
 
 	// insert job info
-	_, err = s.db.Exec("INSERT INTO jobs (job_name, job_info) VALUES (?, ?)", jobName, jobInfo)
-	return err
+	if _, err := s.db.Exec("INSERT INTO jobs (job_name, job_info) VALUES (?, ?)", jobName, jobInfo); err != nil {
+		return errors.Wrapf(err, "insert job name %s failed", jobName)
+	} else {
+		return nil
+	}
 }
 
 // Update Job
 func (s *SQLiteDB) UpdateJob(jobName string, jobInfo string) error {
 	// check job name exists, if not exists, return error
 	var count int
-	err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count)
-	if err != nil {
-		return err
+	if err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count); err != nil {
+		return errors.Wrapf(err, "query job name %s failed", jobName)
 	}
+
 	if count == 0 {
 		return ErrJobNotExists
 	}
 
 	// update job info
-	_, err = s.db.Exec("UPDATE jobs SET job_info = ? WHERE job_name = ?", jobInfo, jobName)
-	return err
+	if _, err := s.db.Exec("UPDATE jobs SET job_info = ? WHERE job_name = ?", jobInfo, jobName); err != nil {
+		return errors.Wrapf(err, "update job name %s failed", jobName)
+	} else {
+		return nil
+	}
 }
 
 func (s *SQLiteDB) IsJobExist(jobName string) (bool, error) {
 	var count int
-	err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count)
-	if err != nil {
-		return false, err
+	if err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count); err != nil {
+		return false, errors.Wrapf(err, "query job name %s failed", jobName)
+	} else {
+		return count > 0, nil
 	}
-	return count > 0, nil
 }
 
 func (s *SQLiteDB) GetAllJobs() (map[string]string, error) {
 	rows, err := s.db.Query("SELECT job_name, job_info FROM jobs")
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, "query all jobs failed")
 	}
 	defer rows.Close()
 
@@ -83,7 +90,7 @@ func (s *SQLiteDB) GetAllJobs() (map[string]string, error) {
 		var jobName, jobInfo string
 		err = rows.Scan(&jobName, &jobInfo)
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrapf(err, "scan job info failed")
 		}
 		result[jobName] = jobInfo
 	}
@@ -93,33 +100,34 @@ func (s *SQLiteDB) GetAllJobs() (map[string]string, error) {
 func (s *SQLiteDB) UpdateProgress(jobName string, progress string) error {
 	// check job name exists, if not exists, return error
 	var count int
-	err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count)
-	if err != nil {
-		return err
+	if err := s.db.QueryRow("SELECT COUNT(*) FROM jobs WHERE job_name = ?", jobName).Scan(&count); err != nil {
+		return errors.Wrapf(err, "query job name %s failed", jobName)
 	}
+
 	if count == 0 {
 		return ErrJobNotExists
 	}
 
 	// update progress
-	_, err = s.db.Exec("UPDATE progresses SET progress = ? WHERE job_name = ?", progress, jobName)
-	return err
+	if _, err := s.db.Exec("UPDATE progresses SET progress = ? WHERE job_name = ?", progress, jobName); err != nil {
+		return errors.Wrapf(err, "update progress failed")
+	} else {
+		return nil
+	}
 }
 
 func (s *SQLiteDB) IsProgressExist(jobName string) (bool, error) {
 	var count int
-	err := s.db.QueryRow("SELECT COUNT(*) FROM progresses WHERE job_name = ?", jobName).Scan(&count)
-	if err != nil {
-		return false, err
+	if err := s.db.QueryRow("SELECT COUNT(*) FROM progresses WHERE job_name = ?", jobName).Scan(&count); err != nil {
+		return false, errors.Wrapf(err, "query job name %s failed", jobName)
 	}
 	return count > 0, nil
 }
 
 func (s *SQLiteDB) GetProgress(jobName string) (string, error) {
 	var progress string
-	err := s.db.QueryRow("SELECT progress FROM progresses WHERE job_name = ?", jobName).Scan(&progress)
-	if err != nil {
-		return "", err
+	if err := s.db.QueryRow("SELECT progress FROM progresses WHERE job_name = ?", jobName).Scan(&progress); err != nil {
+		return "", errors.Wrapf(err, "query progress failed")
 	}
 	return progress, nil
 }
