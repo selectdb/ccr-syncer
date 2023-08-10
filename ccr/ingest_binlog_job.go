267ea8afb287fc3d1591225c866069d61412342a
Jack Drogon <jack.xsuperman@gmail.com>
2023-08-10 16:16:32 +0800

    Split IngestBinlogJob handleTablet
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- ccr/ingest_binlog_job.go
+++ ccr/ingest_binlog_job.go
@@ -30,14 +30,20 @@ func NewIngestContext(txnId int64, tableRecords []*record.TableRecord) *IngestCo
 }
 
 type IngestBinlogJob struct {
-	job             *Job // ccr job
-	txnId           int64
-	tableRecords    []*record.TableRecord
+	job          *Job // ccr job
+	txnId        int64
+	tableRecords []*record.TableRecord
+
+	srcBackendMap  map[int64]*base.Backend
+	destBackendMap map[int64]*base.Backend
+
 	commitInfos     []*ttypes.TTabletCommitInfo
 	commitInfosLock sync.Mutex
-	err             error
-	errLock         sync.RWMutex
-	wg              sync.WaitGroup
+
+	err     error
+	errLock sync.RWMutex
+
+	wg sync.WaitGroup
 }
 
 func NewIngestBinlogJob(ctx context.Context, job *Job) (*IngestBinlogJob, error) {
@@ -87,25 +93,111 @@ func (j *IngestBinlogJob) CommitInfos() []*ttypes.TTabletCommitInfo {
 	return j.commitInfos
 }
 
-func (j *IngestBinlogJob) run() {
+func (j *IngestBinlogJob) handleTablet(binlogVersion int64, destPartitionId int64, srcTablet *TabletMeta, destTablet *TabletMeta) {
+	destTablet.ReplicaMetas.Scan(func(destReplicaId int64, destReplica *ReplicaMeta) bool {
+		log.Debugf("handle dest replica id: %v", destReplicaId)
+		destBackend, ok := j.destBackendMap[destReplica.BackendId]
+		if !ok {
+			j.setError(errors.Errorf("backend not found, backend id: %d", destReplica.BackendId))
+			return false
+		}
+		destTabletId := destReplica.TabletId
+
+		destRpc, err := rpc.NewBeRpc(destBackend)
+		if err != nil {
+			j.setError(err)
+			return false
+		}
+		loadId := ttypes.NewTUniqueId()
+		loadId.SetHi(-1)
+		loadId.SetLo(-1)
+
+		srcReplicas := srcTablet.ReplicaMetas
+		iter := srcReplicas.Iter()
+		if ok := iter.First(); !ok {
+			j.setError(errors.Errorf("src replicas is empty"))
+			return false
+		}
+		srcBackendId := iter.Value().BackendId
+		var srcBackend *base.Backend
+		srcBackend, ok = j.srcBackendMap[srcBackendId]
+		if !ok {
+			j.setError(errors.Errorf("backend not found, backend id: %d", srcBackendId))
+			return false
+		}
+		req := &bestruct.TIngestBinlogRequest{
+			TxnId:          utils.ThriftValueWrapper(j.txnId),
+			RemoteTabletId: utils.ThriftValueWrapper[int64](srcTablet.Id),
+			BinlogVersion:  utils.ThriftValueWrapper(binlogVersion),
+			RemoteHost:     utils.ThriftValueWrapper(srcBackend.Host),
+			RemotePort:     utils.ThriftValueWrapper(srcBackend.GetHttpPortStr()),
+			PartitionId:    utils.ThriftValueWrapper[int64](destPartitionId),
+			LocalTabletId:  utils.ThriftValueWrapper[int64](destTabletId),
+			LoadId:         loadId,
+		}
+		commitInfo := &ttypes.TTabletCommitInfo{
+			TabletId:  destTabletId,
+			BackendId: destBackend.Id,
+		}
+
+		j.wg.Add(1)
+		go func() {
+			defer j.wg.Done()
+
+			resp, err := destRpc.IngestBinlog(req)
+			if err != nil {
+				j.setError(err)
+				return
+			}
+
+			log.Infof("ingest resp: %v", resp)
+			if !resp.IsSetStatus() {
+				err = errors.Errorf("ingest resp status not set")
+				j.setError(err)
+				return
+			} else if resp.Status.StatusCode != tstatus.TStatusCode_OK {
+				err = errors.Errorf("ingest resp status code: %v, msg: %v", resp.Status.StatusCode, resp.Status.ErrorMsgs)
+				j.setError(err)
+				return
+			} else {
+				j.appendCommitInfos(commitInfo)
+			}
+		}()
+
+		return true
+	})
+}
+
+func (j *IngestBinlogJob) prepareBackendMap() error {
 	job := j.job
 
 	var err error
-	var srcBackendMap map[int64]*base.Backend
-	srcBackendMap, err = job.srcMeta.GetBackendMap()
+	j.srcBackendMap, err = job.srcMeta.GetBackendMap()
 	if err != nil {
-		j.setError(err)
-		return
+		return err
 	}
 
-	var destBackendMap map[int64]*base.Backend
-	destBackendMap, err = job.destMeta.GetBackendMap()
+	j.destBackendMap, err = job.destMeta.GetBackendMap()
 	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (j *IngestBinlogJob) run() {
+	job := j.job
+
+	if err := j.prepareBackendMap(); err != nil {
 		j.setError(err)
 		return
 	}
 
 	for _, tableRecord := range j.tableRecords {
+		if j.Error() != nil {
+			return
+		}
+
 		log.Debugf("tableRecord: %v", tableRecord)
 		// TODO: check it before ingestBinlog
 		var srcTableId int64
@@ -170,78 +262,7 @@ func (j *IngestBinlogJob) run() {
 				log.Debugf("handle tablet index: %v, src tablet: %v, dest tablet: %v, dest replicas length: %d", tabletIndex, srcTablet, destTablet, destTablet.ReplicaMetas.Len()) // TODO: remove it
 
 				// iterate dest replicas
-				destTablet.ReplicaMetas.Scan(func(destReplicaId int64, destReplica *ReplicaMeta) bool {
-					log.Debugf("handle dest replica id: %v", destReplicaId)
-					destBackend, ok := destBackendMap[destReplica.BackendId]
-					if !ok {
-						j.setError(errors.Errorf("backend not found, backend id: %d", destReplica.BackendId))
-						return false
-					}
-					destTabletId := destReplica.TabletId
-
-					destRpc, err := rpc.NewBeRpc(destBackend)
-					if err != nil {
-						j.setError(err)
-						return false
-					}
-					loadId := ttypes.NewTUniqueId()
-					loadId.SetHi(-1)
-					loadId.SetLo(-1)
-
-					srcReplicas := srcTablet.ReplicaMetas
-					iter := srcReplicas.Iter()
-					if ok := iter.First(); !ok {
-						j.setError(errors.Errorf("src replicas is empty"))
-						return false
-					}
-					srcBackendId := iter.Value().BackendId
-					var srcBackend *base.Backend
-					srcBackend, ok = srcBackendMap[srcBackendId]
-					if !ok {
-						j.setError(errors.Errorf("backend not found, backend id: %d", srcBackendId))
-						return false
-					}
-					req := &bestruct.TIngestBinlogRequest{
-						TxnId:          utils.ThriftValueWrapper(j.txnId),
-						RemoteTabletId: utils.ThriftValueWrapper[int64](srcTablet.Id),
-						BinlogVersion:  utils.ThriftValueWrapper(binlogVersion),
-						RemoteHost:     utils.ThriftValueWrapper(srcBackend.Host),
-						RemotePort:     utils.ThriftValueWrapper(srcBackend.GetHttpPortStr()),
-						PartitionId:    utils.ThriftValueWrapper[int64](destPartitionId),
-						LocalTabletId:  utils.ThriftValueWrapper[int64](destTabletId),
-						LoadId:         loadId,
-					}
-					commitInfo := &ttypes.TTabletCommitInfo{
-						TabletId:  destTabletId,
-						BackendId: destBackend.Id,
-					}
-
-					j.wg.Add(1)
-					go func() {
-						defer j.wg.Done()
-
-						resp, err := destRpc.IngestBinlog(req)
-						if err != nil {
-							j.setError(err)
-							return
-						}
-
-						log.Infof("ingest resp: %v", resp)
-						if !resp.IsSetStatus() {
-							err = errors.Errorf("ingest resp status not set")
-							j.setError(err)
-							return
-						} else if resp.Status.StatusCode != tstatus.TStatusCode_OK {
-							err = errors.Errorf("ingest resp status code: %v, msg: %v", resp.Status.StatusCode, resp.Status.ErrorMsgs)
-							j.setError(err)
-							return
-						} else {
-							j.appendCommitInfos(commitInfo)
-						}
-					}()
-
-					return true
-				})
+				j.handleTablet(binlogVersion, destPartitionId, srcTablet, destTablet)
 				if j.Error() != nil {
 					break
 				}
