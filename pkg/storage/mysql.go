fdb06a0618ec9f11df588a63ab10655c990033f0
Jack Drogon <jack.xsuperman@gmail.com>
2023-10-18 14:34:05 +0800

    Fix progress from TEXT to LONGTEXT && Update error format
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- pkg/storage/mysql.go
+++ pkg/storage/mysql.go
@@ -18,29 +18,29 @@ type MysqlDB struct {
 func NewMysqlDB(host string, port int, user string, password string) (DB, error) {
 	dbForDDL, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/", user, password, host, port))
 	if err != nil {
-		return nil, xerror.Wrapf(err, xerror.Normal, "open mysql %s@tcp(%s:%s) failed", user, host, password)
+		return nil, xerror.Wrapf(err, xerror.DB, "mysql: open %s@tcp(%s:%s) failed", user, host, password)
 	}
 
 	if _, err := dbForDDL.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", remoteDBName)); err != nil {
-		return nil, xerror.Wrapf(err, xerror.Normal, "create database %s failed", remoteDBName)
+		return nil, xerror.Wrapf(err, xerror.DB, "mysql: create database %s failed", remoteDBName)
 	}
 	dbForDDL.Close()
 
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, port, remoteDBName))
 	if err != nil {
-		return nil, xerror.Wrapf(err, xerror.Normal, "open mysql in db %s@tcp(%s:%d)/%s failed", user, host, port, remoteDBName)
+		return nil, xerror.Wrapf(err, xerror.DB, "mysql: open mysql in db %s@tcp(%s:%d)/%s failed", user, host, port, remoteDBName)
 	}
 
 	if _, err = db.Exec("CREATE TABLE IF NOT EXISTS jobs (`job_name` VARCHAR(512) PRIMARY KEY, `job_info` TEXT, `belong_to` VARCHAR(96))"); err != nil {
-		return nil, xerror.Wrap(err, xerror.Normal, "create table jobs failed")
+		return nil, xerror.Wrap(err, xerror.DB, "mysql: create table jobs failed")
 	}
 
-	if _, err = db.Exec("CREATE TABLE IF NOT EXISTS progresses (`job_name` VARCHAR(512) PRIMARY KEY, `progress` TEXT)"); err != nil {
-		return nil, xerror.Wrap(err, xerror.Normal, "create table progresses failed")
+	if _, err = db.Exec("CREATE TABLE IF NOT EXISTS progresses (`job_name` VARCHAR(512) PRIMARY KEY, `progress` LONGTEXT)"); err != nil {
+		return nil, xerror.Wrap(err, xerror.DB, "mysql: create table progresses failed")
 	}
 
 	if _, err = db.Exec("CREATE TABLE IF NOT EXISTS syncers (`host_info` VARCHAR(96) PRIMARY KEY, `timestamp` BIGINT)"); err != nil {
-		return nil, xerror.Wrap(err, xerror.Normal, "create table syncers failed")
+		return nil, xerror.Wrap(err, xerror.DB, "mysql: create table syncers failed")
 	}
 
 	return &MysqlDB{db: db}, nil
@@ -50,7 +50,7 @@ func (s *MysqlDB) AddJob(jobName string, jobInfo string, hostInfo string) error 
 	// check job name exists, if exists, return error
 	var count int
 	if err := s.db.QueryRow(fmt.Sprintf("SELECT COUNT(*) FROM jobs WHERE job_name = '%s'", jobName)).Scan(&count); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "query job name %s failed", jobName)
+		return xerror.Wrapf(err, xerror.DB, "mysql: query job name %s failed", jobName)
 	}
 
 	if count > 0 {
@@ -60,7 +60,7 @@ func (s *MysqlDB) AddJob(jobName string, jobInfo string, hostInfo string) error 
 	// insert job info
 	insertSql := fmt.Sprintf("INSERT INTO jobs (job_name, job_info, belong_to) VALUES ('%s', '%s', '%s')", jobName, jobInfo, hostInfo)
 	if _, err := s.db.Exec(insertSql); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "insert job name %s failed", jobName)
+		return xerror.Wrapf(err, xerror.DB, "mysql: insert job name %s failed", jobName)
 	} else {
 		return nil
 	}
@@ -71,7 +71,7 @@ func (s *MysqlDB) UpdateJob(jobName string, jobInfo string) error {
 	// check job name exists, if not exists, return error
 	var count int
 	if err := s.db.QueryRow(fmt.Sprintf("SELECT COUNT(*) FROM jobs WHERE job_name = '%s'", jobName)).Scan(&count); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "query job name %s failed", jobName)
+		return xerror.Wrapf(err, xerror.DB, "mysql: query job name %s failed", jobName)
 	}
 
 	if count == 0 {
@@ -80,7 +80,7 @@ func (s *MysqlDB) UpdateJob(jobName string, jobInfo string) error {
 
 	// update job info
 	if _, err := s.db.Exec(fmt.Sprintf("UPDATE jobs SET job_info = '%s' WHERE job_name = '%s'", jobInfo, jobName)); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "update job name %s failed", jobName)
+		return xerror.Wrapf(err, xerror.DB, "mysql: update job name %s failed", jobName)
 	} else {
 		return nil
 	}
@@ -92,25 +92,25 @@ func (s *MysqlDB) RemoveJob(jobName string) error {
 		ReadOnly:  false,
 	})
 	if err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "remove job begin transaction failed, name: %s", jobName)
+		return xerror.Wrapf(err, xerror.DB, "mysql: remove job begin transaction failed, name: %s", jobName)
 	}
 
 	if _, err := txn.Exec(fmt.Sprintf("DELETE FROM jobs WHERE job_name = '%s'", jobName)); err != nil {
 		if err := txn.Rollback(); err != nil {
-			return xerror.Wrapf(err, xerror.Normal, "remove job failed, name: %s, and rollback failed too", jobName)
+			return xerror.Wrapf(err, xerror.DB, "mysql: remove job failed, name: %s, and rollback failed too", jobName)
 		}
-		return xerror.Wrapf(err, xerror.Normal, "remove job failed, name: %s", jobName)
+		return xerror.Wrapf(err, xerror.DB, "mysql: remove job failed, name: %s", jobName)
 	}
 
 	if _, err := txn.Exec(fmt.Sprintf("DELETE FROM progresses WHERE job_name = '%s'", jobName)); err != nil {
 		if err := txn.Rollback(); err != nil {
-			return xerror.Wrapf(err, xerror.Normal, "remove progresses failed, name: %s, and rollback failed too", jobName)
+			return xerror.Wrapf(err, xerror.DB, "mysql: remove progresses failed, name: %s, and rollback failed too", jobName)
 		}
-		return xerror.Wrapf(err, xerror.Normal, "remove progresses failed, name: %s", jobName)
+		return xerror.Wrapf(err, xerror.DB, "mysql: remove progresses failed, name: %s", jobName)
 	}
 
 	if err := txn.Commit(); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "remove job txn commit failed.")
+		return xerror.Wrapf(err, xerror.DB, "mysql: remove job txn commit failed.")
 	}
 
 	return nil
@@ -119,7 +119,7 @@ func (s *MysqlDB) RemoveJob(jobName string) error {
 func (s *MysqlDB) IsJobExist(jobName string) (bool, error) {
 	var count int
 	if err := s.db.QueryRow(fmt.Sprintf("SELECT COUNT(*) FROM jobs WHERE job_name = '%s'", jobName)).Scan(&count); err != nil {
-		return false, xerror.Wrapf(err, xerror.Normal, "query job name %s failed", jobName)
+		return false, xerror.Wrapf(err, xerror.DB, "mysql: query job name %s failed", jobName)
 	} else {
 		return count > 0, nil
 	}
@@ -128,7 +128,7 @@ func (s *MysqlDB) IsJobExist(jobName string) (bool, error) {
 func (s *MysqlDB) GetJobInfo(jobName string) (string, error) {
 	var jobInfo string
 	if err := s.db.QueryRow(fmt.Sprintf("SELECT job_info FROM jobs WHERE job_name = '%s'", jobName)).Scan(&jobInfo); err != nil {
-		return "", xerror.Wrapf(err, xerror.Normal, "get job failed, name: %s", jobName)
+		return "", xerror.Wrapf(err, xerror.DB, "mysql: get job failed, name: %s", jobName)
 	}
 	return jobInfo, nil
 }
@@ -136,7 +136,7 @@ func (s *MysqlDB) GetJobInfo(jobName string) (string, error) {
 func (s *MysqlDB) GetJobBelong(jobName string) (string, error) {
 	var belong string
 	if err := s.db.QueryRow(fmt.Sprintf("SELECT belong_to FROM jobs WHERE job_name = '%s'", jobName)).Scan(&belong); err != nil {
-		return "", xerror.Wrapf(err, xerror.Normal, "get job belong failed, name: %s", jobName)
+		return "", xerror.Wrapf(err, xerror.DB, "mysql: get job belong failed, name: %s", jobName)
 	}
 	return belong, nil
 }
@@ -146,11 +146,11 @@ func (s *MysqlDB) UpdateProgress(jobName string, progress string) error {
 	encodeProgress := base64.StdEncoding.EncodeToString([]byte(progress))
 	updateSql := fmt.Sprintf("INSERT INTO progresses (job_name, progress) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE progress = VALUES(progress)", jobName, encodeProgress)
 	if result, err := s.db.Exec(updateSql); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "update progress failed")
+		return xerror.Wrapf(err, xerror.DB, "mysql: update progress failed")
 	} else if rowNum, err := result.RowsAffected(); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "update progress get affected rows failed")
+		return xerror.Wrapf(err, xerror.DB, "mysql: update progress get affected rows failed")
 	} else if rowNum != 1 {
-		return xerror.Wrapf(err, xerror.Normal, "update progress affected rows error, rows: %d", rowNum)
+		return xerror.Wrapf(err, xerror.DB, "mysql: update progress affected rows error, rows: %d", rowNum)
 	}
 
 	return nil
@@ -159,7 +159,7 @@ func (s *MysqlDB) UpdateProgress(jobName string, progress string) error {
 func (s *MysqlDB) IsProgressExist(jobName string) (bool, error) {
 	var count int
 	if err := s.db.QueryRow(fmt.Sprintf("SELECT COUNT(*) FROM progresses WHERE job_name = '%s'", jobName)).Scan(&count); err != nil {
-		return false, xerror.Wrapf(err, xerror.Normal, "query job name %s failed", jobName)
+		return false, xerror.Wrapf(err, xerror.DB, "mysql: query job name %s failed", jobName)
 	}
 	return count > 0, nil
 }
@@ -167,11 +167,11 @@ func (s *MysqlDB) IsProgressExist(jobName string) (bool, error) {
 func (s *MysqlDB) GetProgress(jobName string) (string, error) {
 	var progress string
 	if err := s.db.QueryRow(fmt.Sprintf("SELECT progress FROM progresses WHERE job_name = '%s'", jobName)).Scan(&progress); err != nil {
-		return "", xerror.Wrapf(err, xerror.Normal, "query progress failed")
+		return "", xerror.Wrapf(err, xerror.DB, "mysql: query progress failed")
 	}
 	decodeProgress, err := base64.StdEncoding.DecodeString(progress)
 	if err != nil {
-		return "", xerror.Errorf(xerror.Normal, "base64 decode error")
+		return "", xerror.Errorf(xerror.DB, "mysql: base64 decode error")
 	}
 	return string(decodeProgress), nil
 }
@@ -180,11 +180,11 @@ func (s *MysqlDB) AddSyncer(hostInfo string) error {
 	timestamp := time.Now().UnixNano()
 	addSql := fmt.Sprintf("INSERT INTO syncers (host_info, timestamp) VALUES ('%s', %d) ON DUPLICATE KEY UPDATE timestamp = VALUES(timestamp)", hostInfo, timestamp)
 	if result, err := s.db.Exec(addSql); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "add syncer failed")
+		return xerror.Wrapf(err, xerror.DB, "mysql: add syncer failed")
 	} else if rowNum, err := result.RowsAffected(); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "add syncer get affected rows failed")
+		return xerror.Wrapf(err, xerror.DB, "mysql: add syncer get affected rows failed")
 	} else if rowNum != 1 {
-		return xerror.Wrapf(err, xerror.Normal, "add syncer affected rows error, rows: %d", rowNum)
+		return xerror.Wrapf(err, xerror.DB, "mysql: add syncer affected rows error, rows: %d", rowNum)
 	}
 
 	return nil
@@ -195,11 +195,11 @@ func (s *MysqlDB) RefreshSyncer(hostInfo string, lastStamp int64) (int64, error)
 	refreshSql := fmt.Sprintf("UPDATE syncers SET timestamp = %d WHERE host_info = '%s' AND timestamp = %d", nowTime, hostInfo, lastStamp)
 	result, err := s.db.Exec(refreshSql)
 	if err != nil {
-		return -1, xerror.Wrapf(err, xerror.Normal, "refresh syncer failed.")
+		return -1, xerror.Wrapf(err, xerror.DB, "mysql: refresh syncer failed.")
 	}
 
 	if rowNum, err := result.RowsAffected(); err != nil {
-		return -1, xerror.Wrapf(err, xerror.Normal, "get RowsAffected failed.")
+		return -1, xerror.Wrapf(err, xerror.DB, "mysql: get RowsAffected failed.")
 	} else if rowNum != 1 {
 		return -1, nil
 	} else {
@@ -213,31 +213,31 @@ func (s *MysqlDB) GetStampAndJobs(hostInfo string) (int64, []string, error) {
 		ReadOnly:  true,
 	})
 	if err != nil {
-		return -1, nil, xerror.Wrapf(err, xerror.Normal, "begin IMMEDIATE transaction failed.")
+		return -1, nil, xerror.Wrapf(err, xerror.DB, "mysql: begin IMMEDIATE transaction failed.")
 	}
 
 	var timestamp int64
 	if err := txn.QueryRow(fmt.Sprintf("SELECT timestamp FROM syncers WHERE host_info = '%s'", hostInfo)).Scan(&timestamp); err != nil {
-		return -1, nil, xerror.Wrapf(err, xerror.Normal, "get stamp failed.")
+		return -1, nil, xerror.Wrapf(err, xerror.DB, "mysql: get stamp failed.")
 	}
 
 	jobs := make([]string, 0)
 	rows, err := s.db.Query(fmt.Sprintf("SELECT job_name FROM jobs WHERE belong_to = '%s'", hostInfo))
 	if err != nil {
-		return -1, nil, xerror.Wrapf(err, xerror.Normal, "get job_nums failed.")
+		return -1, nil, xerror.Wrapf(err, xerror.DB, "mysql: get job_nums failed.")
 	}
 	defer rows.Close()
 
 	for rows.Next() {
 		var jobName string
 		if err := rows.Scan(&jobName); err != nil {
-			return -1, nil, xerror.Wrapf(err, xerror.Normal, "scan job_name failed.")
+			return -1, nil, xerror.Wrapf(err, xerror.DB, "mysql: scan job_name failed.")
 		}
 		jobs = append(jobs, jobName)
 	}
 
 	if err := txn.Commit(); err != nil {
-		return -1, nil, xerror.Wrapf(err, xerror.Normal, "get jobs & stamp txn commit failed.")
+		return -1, nil, xerror.Wrapf(err, xerror.DB, "mysql: get jobs & stamp txn commit failed.")
 	}
 
 	return timestamp, jobs, nil
@@ -246,14 +246,14 @@ func (s *MysqlDB) GetStampAndJobs(hostInfo string) (int64, []string, error) {
 func (s *MysqlDB) GetDeadSyncers(expiredTime int64) ([]string, error) {
 	row, err := s.db.Query(fmt.Sprintf("SELECT host_info FROM syncers WHERE timestamp < %d", expiredTime))
 	if err != nil {
-		return nil, xerror.Wrapf(err, xerror.Normal, "get orphan job info failed.")
+		return nil, xerror.Wrapf(err, xerror.DB, "mysql: get orphan job info failed.")
 	}
 	defer row.Close()
 	deadSyncers := make([]string, 0)
 	for row.Next() {
 		var hostInfo string
 		if err := row.Scan(&hostInfo); err != nil {
-			return nil, xerror.Wrapf(err, xerror.Normal, "scan host_info and jobs failed")
+			return nil, xerror.Wrapf(err, xerror.DB, "mysql: scan host_info and jobs failed")
 		}
 		deadSyncers = append(deadSyncers, hostInfo)
 	}
@@ -265,20 +265,20 @@ func (s *MysqlDB) getOrphanJobs(txn *sql.Tx, syncers []string) ([]string, error)
 	for _, deadSyncer := range syncers {
 		rows, err := txn.Query(fmt.Sprintf("SELECT job_name FROM jobs WHERE belong_to = '%s'", deadSyncer))
 		if err != nil {
-			return nil, xerror.Wrapf(err, xerror.Normal, "get orphan jobs failed.")
+			return nil, xerror.Wrapf(err, xerror.DB, "mysql: get orphan jobs failed.")
 		}
 
 		for rows.Next() {
 			var jobName string
 			if err := rows.Scan(&jobName); err != nil {
-				return nil, xerror.Wrapf(err, xerror.Normal, "scan orphan job name failed.")
+				return nil, xerror.Wrapf(err, xerror.DB, "mysql: scan orphan job name failed.")
 			}
 			orphanJobs = append(orphanJobs, jobName)
 		}
 		rows.Close()
 
 		if _, err := txn.Exec(fmt.Sprintf("DELETE FROM syncers WHERE host_info = '%s'", deadSyncer)); err != nil {
-			return nil, xerror.Wrapf(err, xerror.Normal, "delete dead syncer failed, name: %s", deadSyncer)
+			return nil, xerror.Wrapf(err, xerror.DB, "mysql: delete dead syncer failed, name: %s", deadSyncer)
 		}
 	}
 	return orphanJobs, nil
@@ -289,12 +289,12 @@ func (s *MysqlDB) getLoadInfo(txn *sql.Tx) (LoadSlice, int, error) {
 	sumLoad := 0
 	host_rows, err := txn.Query("SELECT host_info FROM syncers")
 	if err != nil {
-		return nil, -1, xerror.Wrapf(err, xerror.Normal, "get all syncers failed.")
+		return nil, -1, xerror.Wrapf(err, xerror.DB, "mysql: get all syncers failed.")
 	}
 	for host_rows.Next() {
 		loadInfo := LoadInfo{AddedLoad: 0}
 		if err := host_rows.Scan(&loadInfo.HostInfo); err != nil {
-			return nil, -1, xerror.Wrapf(err, xerror.Normal, "scan load info failed.")
+			return nil, -1, xerror.Wrapf(err, xerror.DB, "mysql: scan load info failed.")
 		}
 		load = append(load, loadInfo)
 	}
@@ -302,7 +302,7 @@ func (s *MysqlDB) getLoadInfo(txn *sql.Tx) (LoadSlice, int, error) {
 
 	for i := range load {
 		if err := txn.QueryRow(fmt.Sprintf("SELECT COUNT(*) FROM jobs WHERE belong_to = '%s'", load[i].HostInfo)).Scan(&load[i].NowLoad); err != nil {
-			return nil, -1, xerror.Wrapf(err, xerror.Normal, "get syncer %s load failed.", load[i].HostInfo)
+			return nil, -1, xerror.Wrapf(err, xerror.DB, "mysql: get syncer %s load failed.", load[i].HostInfo)
 		}
 		sumLoad += load[i].NowLoad
 	}
@@ -313,11 +313,11 @@ func (s *MysqlDB) getLoadInfo(txn *sql.Tx) (LoadSlice, int, error) {
 func (s *MysqlDB) dispatchJobs(txn *sql.Tx, hostInfo string, additionalJobs []string) error {
 	for _, jobName := range additionalJobs {
 		if _, err := txn.Exec(fmt.Sprintf("UPDATE jobs SET belong_to = '%s' WHERE job_name = '%s'", hostInfo, jobName)); err != nil {
-			return xerror.Wrapf(err, xerror.Normal, "update job belong_to failed, name: %s", jobName)
+			return xerror.Wrapf(err, xerror.DB, "mysql: update job belong_to failed, name: %s", jobName)
 		}
 	}
 	if _, err := txn.Exec(fmt.Sprintf("UPDATE syncers SET timestamp = %d WHERE host_info = '%s'", time.Now().UnixNano(), hostInfo)); err != nil {
-		return xerror.Wrapf(err, xerror.Normal, "update syncer timestamp failed, host: %s", hostInfo)
+		return xerror.Wrapf(err, xerror.DB, "mysql: update syncer timestamp failed, host: %s", hostInfo)
 	}
 	return nil
 }
@@ -328,7 +328,7 @@ func (s *MysqlDB) RebalanceLoadFromDeadSyncers(syncers []string) error {
 		ReadOnly:  false,
 	})
 	if err != nil {
-		return xerror.Wrap(err, xerror.Normal, "rebalance load begin txn failed")
+		return xerror.Wrap(err, xerror.DB, "mysql: rebalance load begin txn failed")
 	}
 
 	orphanJobs, err := s.getOrphanJobs(txn, syncers)
@@ -350,7 +350,7 @@ func (s *MysqlDB) RebalanceLoadFromDeadSyncers(syncers []string) error {
 		beginIdx := additionalLoad - loadList[i].AddedLoad
 		if err := s.dispatchJobs(txn, loadList[i].HostInfo, orphanJobs[beginIdx:additionalLoad]); err != nil {
 			if err := txn.Rollback(); err != nil {
-				return xerror.Wrap(err, xerror.Normal, "rebalance rollback failed.")
+				return xerror.Wrap(err, xerror.DB, "mysql: rebalance rollback failed.")
 			}
 			return err
 		}
@@ -358,7 +358,7 @@ func (s *MysqlDB) RebalanceLoadFromDeadSyncers(syncers []string) error {
 	}
 
 	if err := txn.Commit(); err != nil {
-		return xerror.Wrap(err, xerror.Normal, "rebalance txn commit failed.")
+		return xerror.Wrap(err, xerror.DB, "mysql: rebalance txn commit failed.")
 	}
 
 	return nil
@@ -369,14 +369,14 @@ func (s *MysqlDB) GetAllData() (map[string][]string, error) {
 
 	jobRows, err := s.db.Query("SELECT job_name, belong_to FROM jobs")
 	if err != nil {
-		return nil, xerror.Wrap(err, xerror.Normal, "get jobs data failed.")
+		return nil, xerror.Wrap(err, xerror.DB, "mysql: get jobs data failed.")
 	}
 	jobData := make([]string, 0)
 	for jobRows.Next() {
 		var jobName string
 		var belongTo string
 		if err := jobRows.Scan(&jobName, &belongTo); err != nil {
-			return nil, xerror.Wrap(err, xerror.Normal, "scan jobs row failed.")
+			return nil, xerror.Wrap(err, xerror.DB, "mysql: scan jobs row failed.")
 		}
 		jobData = append(jobData, fmt.Sprintf("%s, %s", jobName, belongTo))
 	}
@@ -385,7 +385,7 @@ func (s *MysqlDB) GetAllData() (map[string][]string, error) {
 
 	syncerRows, err := s.db.Query("SELECT * FROM syncers")
 	if err != nil {
-		return nil, xerror.Wrap(err, xerror.Normal, "get jobs data failed.")
+		return nil, xerror.Wrap(err, xerror.DB, "mysql: get jobs data failed.")
 	}
 
 	syncerData := make([]string, 0)
@@ -393,7 +393,7 @@ func (s *MysqlDB) GetAllData() (map[string][]string, error) {
 		var hostInfo string
 		var timestamp int64
 		if err := syncerRows.Scan(&hostInfo, &timestamp); err != nil {
-			return nil, xerror.Wrap(err, xerror.Normal, "scan syncers row failed.")
+			return nil, xerror.Wrap(err, xerror.DB, "mysql: scan syncers row failed.")
 		}
 		syncerData = append(syncerData, fmt.Sprintf("%s, %d", hostInfo, timestamp))
 	}
