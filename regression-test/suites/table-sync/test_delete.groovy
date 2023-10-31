5f4a64da581ecdb81ee454e3353e794a49ae1e26
lsy3993 <xm610487344@hotmail.com>
2023-10-31 16:49:03 +0800

    add delete regression test

---
+++ regression-test/suites/table-sync/test_delete.groovy
@@ -0,0 +1,109 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_delete") {
+    def tableName = "tbl_rename_" + UUID.randomUUID().toString().replace("-", "")
+    def syncerAddress = "127.0.0.1:9190"
+    def test_num = 0
+    def insert_num = 5
+    def sync_gap_time = 5000
+    String respone
+
+    def checkSelectTimesOf = { sqlString, rowSize, times -> Boolean
+        def tmpRes = target_sql "${sqlString}"
+        while (tmpRes.size() != rowSize) {
+            sleep(sync_gap_time)
+            if (--times > 0) {
+                tmpRes = target_sql "${sqlString}"
+            } else {
+                break
+            }
+        }
+        return tmpRes.size() == rowSize
+    }
+
+    def checkRestoreFinishTimesOf = { checkTable, times -> Boolean
+        Boolean ret = false
+        while (times > 0) {
+            def sqlInfo = target_sql "SHOW RESTORE FROM TEST_${context.dbName}"
+            for (List<Object> row : sqlInfo) {
+                if ((row[10] as String).contains(checkTable)) {
+                    ret = (row[4] as String) == "FINISHED"
+                }
+            }
+
+            if (ret) {
+                break
+            } else if (--times > 0) {
+                sleep(sync_gap_time)
+            }
+        }
+
+        return ret
+    }
+
+    sql """
+        CREATE TABLE if NOT EXISTS ${tableName}
+        (
+            `test` INT,
+            `id` INT
+        )
+        ENGINE=OLAP
+        UNIQUE KEY(`test`, `id`)
+        PARTITION BY RANGE(`test`)
+        (
+            PARTITION `less100` VALUES LESS THAN ("100")
+        )
+        DISTRIBUTED BY HASH(id) BUCKETS 1
+        PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "binlog.enable" = "true"
+        )
+    """
+    sql """ALTER TABLE ${tableName} set ("binlog.enable" = "true")"""
+
+    httpTest {
+        uri "/create_ccr"
+        endpoint syncerAddress
+        def bodyJson = get_ccr_body "${tableName}"
+        body "${bodyJson}"
+        op "post"
+        result respone
+    }
+
+    assertTrue(checkRestoreFinishTimesOf("${tableName}", 30))
+
+
+    logger.info("=== Test 0: Common insert case ===")
+    for (int index = 0; index < insert_num; index++) {
+        sql """
+            INSERT INTO ${tableName} VALUES (${test_num}, ${index})
+            """
+    }
+    assertTrue(checkSelectTimesOf("SELECT * FROM ${tableName} WHERE test=${test_num}",
+                                  insert_num, 30))
+
+
+
+    logger.info("=== Test 1: delete row case ===")
+    test_num = 1
+    sql "DELETE FROM ${tableName} WHERE test = ${test_num} and id = 0"
+
+    // insert 5 rows, delete 2 rows, 'select test from TEST_${context.dbName}.${tableName}' should return 3 rows
+    assertTrue(checkSelectTimesOf("SELECT test FROM TEST_${context.dbName}.${tableName}", 3, 30))
+
+}
