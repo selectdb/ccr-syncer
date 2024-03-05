fb7c36efd3b8d349c65cc0448320f733f2291384
Jianliang Qi <jianliang5669@gmail.com>
2024-03-05 20:13:21 +0800

    Add regression test for inverted index table

--- regression-test/suites/table-sync/test_inverted_index.groovy
+++ regression-test/suites/table-sync/test_inverted_index.groovy
@@ -17,12 +17,12 @@
 
 suite("test_inverted_index") {
 
-    def tableName = "tbl_inverted_index_" + UUID.randomUUID().toString().replace("-", "")
+    def tableName = "tbl_inverted_index_dup_" + UUID.randomUUID().toString().replace("-", "")
     def syncerAddress = "127.0.0.1:9190"
     def test_num = 0
     def insert_num = 5
     def sync_gap_time = 5000
-    String response
+    String respone
 
     def checkRestoreFinishTimesOf = { checkTable, times -> Boolean
         Boolean ret = false
@@ -44,46 +44,158 @@ suite("test_inverted_index") {
         return ret
     }
 
-    sql """
-        CREATE TABLE if NOT EXISTS ${tableName}
-        (
-            `test` INT,
-            `id` INT,
-            INDEX idx_id (`id`) USING INVERTED
-        )
-        ENGINE=OLAP
-        DUPLICATE KEY(`test`, `id`)
-        DISTRIBUTED BY HASH(id) BUCKETS 1
-        PROPERTIES (
-            "replication_allocation" = "tag.location.default: 1"
-        )
-    """
-    for (int index = 0; index < insert_num; index++) {
-        sql """
-            INSERT INTO ${tableName} VALUES (${test_num}, ${index})
-            """
-    }
-    sql """ALTER TABLE ${tableName} set ("binlog.enable" = "true")"""
-    sql "sync"
-
-    httpTest {
-        uri "/create_ccr"
-        endpoint syncerAddress
-        def bodyJson = get_ccr_body "${tableName}"
-        body "${bodyJson}"
-        op "post"
-        result response
-    }
-
-    assertTrue(checkRestoreFinishTimesOf("${tableName}", 30))
-
-    def res = target_sql "SHOW INDEXES FROM TEST_${context.dbName}.${tableName}"
-    def invertIdx = false
-    for (List<Object> row : res) {
-        if ((row[2] as String) == "idx_id") {
-            invertIdx = (row[10] as String) == "INVERTED"
-            break
+    def checkSyncFinishTimesOf = { count, times -> Boolean
+        Boolean ret = false
+        while (times > 0) {
+            def sqlInfo = target_sql "SELECT COUNT() FROM TEST_${context.dbName}.${tableName}"
+            if ((sqlInfo[0][0] as Integer) == count) {
+                ret = true
+                break
+            } else if (--times > 0) {
+                sleep(sync_gap_time)
+            }
         }
+
+        return ret
     }
-    assertTrue(invertIdx)
+
+    def insert_data = { -> 
+        sql """ INSERT INTO ${tableName} VALUES (1, "andy", "andy love apple", 100); """
+        sql """ INSERT INTO ${tableName} VALUES (1, "bason", "bason hate pear", 100); """
+        sql """ INSERT INTO ${tableName} VALUES (2, "andy", "andy love apple", 100); """
+        sql """ INSERT INTO ${tableName} VALUES (2, "bason", "bason hate pear", 98); """
+        sql """ INSERT INTO ${tableName} VALUES (3, "andy", "andy love apple", 100); """
+        sql """ INSERT INTO ${tableName} VALUES (3, "bason", "bason hate pear", 99); """
+        sql """ INSERT INTO ${tableName} VALUES (4, "bason", "bason hate pear", 99); """
+        sql """ INSERT INTO ${tableName} VALUES (4, "andy", "andy love apple", 100); """
+    }
+
+    def insert_data2 = { -> 
+        sql """ INSERT INTO ${tableName} VALUES (5, "bason", "bason hate pear", 99); """
+        sql """ INSERT INTO ${tableName} VALUES (5, "andy", "andy love apple", 100); """
+        sql """ INSERT INTO ${tableName} VALUES (6, "bason", "bason hate pear", 99); """
+        sql """ INSERT INTO ${tableName} VALUES (6, "andy", "andy love apple", 98); """
+    }
+
+    def run_sql = { String db -> 
+        qt_sql """ select * from ${db}.${tableName} order by id, name, hobbies, score """
+        qt_sql """ select * from ${db}.${tableName} where name match "andy" order by id, name, hobbies, score """
+        qt_sql """ select * from ${db}.${tableName} where hobbies match "pear" order by id, name, hobbies, score """
+        qt_sql """ select * from ${db}.${tableName} where score < 99 order by id, name, hobbies, score """
+    }
+
+    def run_test = { ->
+        insert_data.call()
+        
+        sql """ALTER TABLE ${tableName} set ("binlog.enable" = "true")"""
+        sql "sync"
+
+        httpTest {
+            uri "/create_ccr"
+            endpoint syncerAddress
+            def bodyJson = get_ccr_body "${tableName}"
+            body "${bodyJson}"
+            op "post"
+            result respone
+        }
+
+        assertTrue(checkRestoreFinishTimesOf("${tableName}", 30))
+
+        def show_result = target_sql "SHOW INDEXES FROM TEST_${context.dbName}.${tableName}"
+        logger.info("show index from TEST_${context.dbName}.${tableName} result: " + show_result)
+        assertEquals(show_result.size(), 3)
+        assertEquals(show_result[0][2], "index_name")
+        assertEquals(show_result[1][2], "index_hobbies")
+        assertEquals(show_result[2][2], "index_score")
+
+        run_sql.call("${context.dbName}")
+        run_sql.call("TEST_${context.dbName}")
+
+        insert_data2.call()
+        sql "sync"
+
+        if (("${tableName}" as String).contains("tbl_inverted_index_dup")) {
+            assertTrue(checkSyncFinishTimesOf(12, 30))
+        } else {
+            assertTrue(checkSyncFinishTimesOf(6, 30))
+        }
+
+        run_sql.call("${context.dbName}")
+        run_sql.call("TEST_${context.dbName}")
+    }
+
+    /**
+    * test for duplicated key table
+    */
+    sql """ DROP TABLE IF EXISTS ${tableName}; """
+    sql """
+        CREATE TABLE ${tableName} (
+            `id` int(11) NULL,
+            `name` varchar(255) NULL,
+            `hobbies` text NULL,
+            `score` int(11) NULL,
+            index index_name (name) using inverted,
+            index index_hobbies (hobbies) using inverted properties("parser"="english"),
+            index index_score (score) using inverted
+        ) ENGINE=OLAP
+        DUPLICATE KEY(`id`)
+        COMMENT 'OLAP'
+        DISTRIBUTED BY HASH(`id`) BUCKETS 1
+        PROPERTIES ( "replication_num" = "1");
+    """
+
+    run_test.call()
+
+    /**
+    * test for unique key table with mow
+    */
+    tableName = "tbl_inverted_index_unique_mow_" + UUID.randomUUID().toString().replace("-", "")
+
+    sql """ DROP TABLE IF EXISTS ${tableName}; """
+    sql """
+        CREATE TABLE ${tableName} (
+            `id` int(11) NULL,
+            `name` varchar(255) NULL,
+            `hobbies` text NULL,
+            `score` int(11) NULL,
+            index index_name (name) using inverted,
+            index index_hobbies (hobbies) using inverted properties("parser"="english"),
+            index index_score (score) using inverted
+        ) ENGINE=OLAP
+        UNIQUE KEY(`id`)
+        COMMENT 'OLAP'
+        DISTRIBUTED BY HASH(`id`) BUCKETS 1
+        PROPERTIES ( 
+            "replication_num" = "1",
+            "enable_unique_key_merge_on_write" = "true"
+        );
+    """
+
+    run_test.call()
+
+    /**
+    * test for unique key table with mor
+    */
+    tableName = "tbl_inverted_index_unique_mor_" + UUID.randomUUID().toString().replace("-", "")
+
+    sql """ DROP TABLE IF EXISTS ${tableName}; """
+    sql """
+        CREATE TABLE ${tableName} (
+            `id` int(11) NULL,
+            `name` varchar(255) NULL,
+            `hobbies` text NULL,
+            `score` int(11) NULL,
+            index index_name (name) using inverted,
+            index index_hobbies (hobbies) using inverted properties("parser"="english"),
+            index index_score (score) using inverted
+        ) ENGINE=OLAP
+        UNIQUE KEY(`id`)
+        COMMENT 'OLAP'
+        DISTRIBUTED BY HASH(`id`) BUCKETS 1
+        PROPERTIES ( 
+            "replication_num" = "1"
+        );
+    """
+
+    run_test.call()
 }
