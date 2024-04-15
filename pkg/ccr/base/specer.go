f033e70c7f745c4ff1320fc457960ebaf7ec8055
w41ter <w41ter.l@gmail.com>
2024-04-15 18:05:00 +0800

    Drop the table if restore is cancelled because table signature not matched

--- pkg/ccr/base/specer.go
+++ pkg/ccr/base/specer.go
@@ -28,6 +28,7 @@ type Specer interface {
 	CheckTableExists() (bool, error)
 	CreateSnapshotAndWaitForDone(tables []string) (string, error)
 	CheckRestoreFinished(snapshotName string) (bool, error)
+	GetRestoreSignatureNotMatchedTable(snapshotName string) (string, error)
 	WaitTransactionDone(txnId int64) // busy wait
 
 	Exec(sql string) error
