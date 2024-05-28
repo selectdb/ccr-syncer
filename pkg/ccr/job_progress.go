selectdb/ccr-syncer

commit dac089faefeb5d5e0ab7f64a0179262716cff154

Fix binlog lost (#86)

pkg/ccr/job_progress.go
@@ -210,7 +210,6 @@ func NewJobProgressFromJson(jobName string, db storage.DB) (*JobProgress, error)
 }
 
 func (j *JobProgress) StartHandle(commitSeq int64) {
-	j.PrevCommitSeq = j.CommitSeq
 	j.CommitSeq = commitSeq
 
 	j.Persist()
@@ -284,7 +283,7 @@ func (j *JobProgress) NextWithPersist(commitSeq int64, syncState SyncState, subS
 	j.Persist()
 }
 
-func (j *JobProgress) IsDone() bool { return j.SubSyncState == Done }
+func (j *JobProgress) IsDone() bool { return j.SubSyncState == Done && j.PrevCommitSeq == j.CommitSeq }
 
 // TODO(Drogon): check reset some fields
 func (j *JobProgress) Done() {
@@ -338,5 +337,6 @@ func (j *JobProgress) Persist() {
 		break
 	}
 
-	log.Trace("update job progress done")
+	log.Tracef("update job progress done, state: %s, subState: %s, commitSeq: %d, prevCommitSeq: %d",
+		j.SyncState, j.SubSyncState, j.CommitSeq, j.PrevCommitSeq)
 }

pkg/ccr/job.go
@@ -1186,7 +1186,8 @@ func (j *Job) recoverIncrementalSync() error {
 
 func (j *Job) incrementalSync() error {
 	if !j.progress.IsDone() {
-		log.Infof("job progress is not done, state is (%s), need recover", j.progress.SubSyncState)
+		log.Infof("job progress is not done, need recover. state: %s, prevCommitSeq: %d, commitSeq: %d",
+			j.progress.SubSyncState, j.progress.PrevCommitSeq, j.progress.CommitSeq)
 
 		return j.recoverIncrementalSync()
 	}
@@ -1202,6 +1203,7 @@ func (j *Job) incrementalSync() error {
 
 	// Step 2: handle all binlog
 	for {
+		// The CommitSeq is equals to PrevCommitSeq in here.
 		commitSeq := j.progress.CommitSeq
 		log.Debugf("src: %s, commitSeq: %v", src, commitSeq)
 

commit 3b38f6785bdbb4b375de0a3f5bf9fa4c97355b80

Save prev commit seq if progress is done
Fix the bug introduced by #86.

The prev commit seq is indicates the commit seq where the target
cluster has synced, so once restoring is finished, the prev commit
seq should be set to aligned with commit seq

pkg/ccr/job_progress.go
@@ -136,6 +136,7 @@ type JobProgress struct {
 	// Sub sync state machine states
 	SubSyncState SubSyncState `json:"sub_sync_state"`
 
+	// The commit seq where the target cluster has synced.
 	PrevCommitSeq     int64           `json:"prev_commit_seq"`
 	CommitSeq         int64           `json:"commit_seq"`
 	TableMapping      map[int64]int64 `json:"table_mapping"`
@@ -264,6 +265,9 @@ func (j *JobProgress) CommitNextSubWithPersist(commitSeq int64, subSyncState Sub
 	j.Persist()
 }
 
+// Switch to new sync state.
+//
+// The PrevCommitSeq is set to commitSeq, if the sub sync state is done.
 func (j *JobProgress) NextWithPersist(commitSeq int64, syncState SyncState, subSyncState SubSyncState, persistData string) {
 	if subSyncState == BeginCreateSnapshot && (syncState == TableFullSync || syncState == DBFullSync) {
 		j.FullSyncStartAt = time.Now().Unix()
@@ -275,6 +279,10 @@ func (j *JobProgress) NextWithPersist(commitSeq int64, syncState SyncState, subS
 	}
 
 	j.CommitSeq = commitSeq
+	if subSyncState == Done {
+		j.PrevCommitSeq = commitSeq
+	}
+
 	j.SyncState = syncState
 	j.SubSyncState = subSyncState
 	j.PersistData = persistData