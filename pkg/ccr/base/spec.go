49b7b99787f2b99941d1ee835e0c59c7202bfcd9
w41ter <w41ter.l@gmail.com>
2024-06-03 19:32:43 +0800

    Remove useless CreateTable

--- pkg/ccr/base/spec.go
+++ pkg/ccr/base/spec.go
@@ -408,18 +408,11 @@ func (s *Spec) CreateDatabase() error {
 	return nil
 }
 
-func (s *Spec) CreateTable(createTable *record.CreateTable) error {
-	sql := createTable.Sql
-	log.Infof("createTableSql: %s", sql)
-	// HACK: for drop table
-	return s.DbExec(sql)
-}
-
 func (s *Spec) CreateTableOrView(createTable *record.CreateTable, srcDatabase string) error {
 	//	Creating table will only occur when sync db.
 	//	When create view, the db name of sql is source db name, we should use dest db name to create view
 	createSql := createTable.Sql
-	viewRegex := regexp.MustCompile("(?i)^CREATE(\\s+)VIEW")
+	viewRegex := regexp.MustCompile(`(?i)^CREATE(\s+)VIEW`)
 	isCreateView := viewRegex.MatchString(createSql)
 	if isCreateView {
 		log.Debugf("create view, use dest db name to replace source db name")
@@ -428,7 +421,7 @@ func (s *Spec) CreateTableOrView(createTable *record.CreateTable, srcDatabase st
 		originalName := "`internal`.`" + strings.TrimSpace(srcDatabase) + "`."
 		replaceName := "`internal`.`" + strings.TrimSpace(s.Database) + "`."
 		createTable.Sql = strings.ReplaceAll(createTable.Sql, originalName, replaceName)
-		log.Debugf("original create view sql is %s, after repalce, now sql is %s", createSql, createTable.Sql)
+		log.Debugf("original create view sql is %s, after replace, now sql is %s", createSql, createTable.Sql)
 	}
 
 	sql := createTable.Sql
