c428f8f03032099b005b519331c0839d119fbe2b
yanmingfu <yanmingfu@baidu.com>
2024-09-24 22:05:35 +0800

    Code Formatting

--- pkg/ccr/job.go
+++ pkg/ccr/job.go
@@ -1674,7 +1674,7 @@ func (j *Job) handleBinlog(binlog *festruct.TBinlog) error {
 	case festruct.TBinlogType_REPLACE_PARTITIONS:
 		return j.handleReplacePartitions(binlog)
 	case festruct.TBinlogType_MODIFY_VIEW_DEF:
-    	return j.handleAlterViewDef(binlog)
+	    return j.handleAlterViewDef(binlog)
 	default:
 		return xerror.Errorf(xerror.Normal, "unknown binlog type: %v", binlog.GetType())
 	}
