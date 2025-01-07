83fa005126599b03a1529fb38e839815b37b813b
Uniqueyou <1520358997@qq.com>
2025-01-07 17:56:12 +0800

    Add test for table sync with alias (#370)

---
+++ regression-test/suites/table_sync_alias/table/duplicate/test_tsa_tbl_duplicate.groovy
@@ -0,0 +1,71 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_tsa_tbl_duplicate") {
+    def helper = new GroovyShell(new Binding(['suite': delegate]))
+            .evaluate(new File("${context.config.suitePath}/../common", "helper.groovy"))
+
+    def dbName = context.dbName
+    def tableName = "tbl_" + helper.randomSuffix()
+    def aliasName = "alias_" + helper.randomSuffix()
+    def test_num = 0
+    def insert_num = 5
+
+    def exist = { res -> Boolean
+        return res.size() != 0
+    }
+
+    helper.set_alias(aliasName)
+
+    sql "DROP TABLE IF EXISTS ${dbName}.${tableName}"
+    target_sql "DROP TABLE IF EXISTS TEST_${dbName}.${aliasName}"
+
+    helper.enableDbBinlog()
+
+    sql """
+        CREATE TABLE if NOT EXISTS ${tableName}
+        (
+            `test` INT,
+            `id` INT
+        )
+        ENGINE=OLAP
+        DUPLICATE KEY(`test`, `id`)
+        PARTITION BY RANGE(`id`)
+        (
+        )
+        DISTRIBUTED BY HASH(id) BUCKETS 1
+        PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "binlog.enable" = "true"
+        )
+    """
+
+    helper.ccrJobDelete(tableName)
+    helper.ccrJobCreate(tableName)
+
+    assertTrue(helper.checkRestoreFinishTimesOf("${tableName}", 30))
+
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 60, "sql"))
+
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${aliasName}\"", exist, 60, "target"))
+
+    def sql_res = sql "SHOW CREATE TABLE ${tableName}"
+
+    def target_res = target_sql "SHOW CREATE TABLE ${aliasName}"
+
+    assertTrue(target_res[0][1].contains("DUPLICATE KEY(`test`, `id`)"))
+}
