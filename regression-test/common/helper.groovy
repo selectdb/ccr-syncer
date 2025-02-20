173549a6a46a4f2ad7e88c7c9eed7e78d8850a55
Uniqueyou <wangyixuan@selectdb.com>
2025-02-20 11:37:54 +0800

    fix: db not enable binlog and ignore e for checkSelectTimesOf (#457)

--- regression-test/common/helper.groovy
+++ regression-test/common/helper.groovy
@@ -351,7 +351,7 @@ class Helper {
                 if (tmpRes.size() == rowSize) {
                     return true
                 }
-            } catch (Exception) {
+            } catch (Exception e) {
                 logger.info("exception", e)
             }
             sleep(sync_gap_time)
