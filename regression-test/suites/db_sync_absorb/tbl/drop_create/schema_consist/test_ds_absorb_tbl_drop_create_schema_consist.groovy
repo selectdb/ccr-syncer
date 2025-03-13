a4afe8a767fcc299f34a99d4d40063f707d770d2
koarz <66543806+koarz@users.noreply.github.com>
2025-03-13 15:15:03 +0800

    test: add drop create schema consist test (#506)

---
+++ regression-test/suites/db_sync_absorb/tbl/drop_create/schema_consist/test_ds_absorb_tbl_drop_create_schema_consist.groovy
@@ -0,0 +1,113 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+suite("test_ds_absorb_tbl_drop_create_schema_consist") {
+    def helper = new GroovyShell(new Binding(['suite': delegate]))
+            .evaluate(new File("${context.config.suitePath}/../common", "helper.groovy"))
+
+    def tableName = "tbl_" + helper.randomSuffix()
+    def test_num = 0
+    def insert_num = 10
+
+    def exist = { res -> Boolean
+        return res.size() != 0
+    }
+    def notExist = { res -> Boolean
+        return res.size() == 0
+    }
+
+    def checkValue = { res, index, expect -> Boolean
+        return res[index][1] == expect
+    }
+
+    sql """
+        CREATE TABLE if NOT EXISTS ${tableName}
+        (
+            `test` INT,
+            `id` INT
+        )
+        ENGINE=OLAP
+        UNIQUE KEY(`test`, `id`)
+        DISTRIBUTED BY HASH(id) BUCKETS 1
+        PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "binlog.enable" = "true"
+        )
+    """
+
+    helper.enableDbBinlog()
+    helper.ccrJobDelete()
+    helper.ccrJobCreate()
+
+    assertTrue(helper.checkRestoreFinishTimesOf("${tableName}", 180))
+    assertTrue(helper.checkShowTimesOf(""" SHOW TABLES LIKE "${tableName}" """, exist, 60, "sql"))
+
+    // 1. Pause ccr job
+    helper.ccrJobPause()
+
+    // 2. Insert N data
+    for (int index = 0; index < insert_num; index++) {
+        sql """
+            INSERT INTO ${tableName} VALUES (${test_num}, ${index})
+            """
+    }
+
+    def res = sql " select * from ${tableName}";
+    for (int index = 0; index < insert_num; index++) {
+        assertTrue(checkValue(res, index, index))
+    }
+    // 3. Do operation & wait it finishes upstream
+    sql "DROP TABLE ${tableName} FORCE"
+    sql """
+        CREATE TABLE if NOT EXISTS ${tableName}
+        (
+            `test` INT,
+            `id` INT
+        )
+        ENGINE=OLAP
+        UNIQUE KEY(`test`, `id`)
+        DISTRIBUTED BY HASH(id) BUCKETS 1
+        PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "binlog.enable" = "true"
+        )
+    """
+
+    // 4. Insert N data
+    for (int index = insert_num; index < insert_num * 2; index++) {
+        sql """
+            INSERT INTO ${tableName} VALUES (${test_num}, ${index})
+            """
+    }
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == insert_num }, 180, "sql"))
+    res = sql " select * from ${tableName}";
+    for (int index = 0; index < insert_num; index++) {
+        assertTrue(checkValue(res, index, index + insert_num))
+    }
+    // 5. Force trigger fullsnapshot
+    helper.force_fullsync()
+
+    // 6. Resume ccr job
+    helper.ccrJobResume()
+  
+    // 7. Verify data and operation are synced downstream
+    assertTrue(helper.checkShowTimesOf(""" SHOW TABLES LIKE "${tableName}" """, exist, 60, "target"))
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == insert_num }, 60, "target"))
+    res = target_sql " select * from ${tableName}";
+    for (int index = 0; index < insert_num; index++) {
+        assertTrue(checkValue(res, index, index + insert_num))
+    }
+}
