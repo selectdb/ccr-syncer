91f2bfc54af60bf7bb5d7faf0d17da6b99c330af
walter <w41ter.l@gmail.com>
2025-03-13 19:02:28 +0800

    fix: partial sync view if unknown column exists (#510)

--- pkg/ccr/utils.go
+++ pkg/ccr/utils.go
@@ -60,6 +60,11 @@ func (i *BackupJobInfo) TableNameMapping() map[int64]string {
 	for tableName, tableInfo := range i.BackupObjects {
 		tableMapping[tableInfo.Id] = tableName
 	}
+	if i.NewBackupObjects != nil {
+		for _, viewInfo := range i.NewBackupObjects.Views {
+			tableMapping[viewInfo.Id] = viewInfo.Name
+		}
+	}
 	return tableMapping
 }
 
@@ -82,4 +87,17 @@ func (i *BackupJobInfo) Views() []string {
 		views = append(views, viewInfo.Name)
 	}
 	return views
+}
+
+func (i *BackupJobInfo) GetView(name string) (*BackupViewInfo, bool) {
+	if i.NewBackupObjects == nil {
+		return nil, false
+	}
+
+	for _, viewInfo := range i.NewBackupObjects.Views {
+		if viewInfo.Name == name {
+			return &viewInfo, true
+		}
+	}
+	return nil, false
 }
