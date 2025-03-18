b3a16b5d941782b1d0cf4051a4dd49b3905b0f99
koarz <lihao@selectdb.com>
2025-03-18 14:41:19 +0800

    test: add absorb ds col test

---
+++ regression-test/suites/db_sync_absorb/col/add_key_mod/test_ds_absorb_col_add_key_mod.groovy
@@ -0,0 +1,138 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+suite("test_ds_absorb_col_add_key_mod") {
+    def helper = new GroovyShell(new Binding(['suite': delegate]))
+            .evaluate(new File("${context.config.suitePath}/../common", "helper.groovy"))
+
+    def dbName = context.dbName
+    def dbNameTarget = "TEST_" + context.dbName
+    def tableName = "tbl_" + helper.randomSuffix()
+    def test_num = 0
+    def test_new_column_num = 1
+    def insert_num = 5
+
+    def exist = { res -> Boolean
+        return res.size() != 0
+    }
+
+    def has_count = { count ->
+        return { res -> Boolean
+            res.size() == count
+        }
+    }
+
+    def value_is_big_int = { res -> Boolean
+        // Field == 'value' && 'Type' == 'bigint'
+        return res[3][0] == 'value' && res[3][1] == 'bigint'
+    }
+
+    def id_is_big_int = { res -> Boolean
+        // Field == 'id' && 'Type' == 'bigint'
+        return res[2][0] == 'id' && res[2][1] == 'bigint'
+    }
+
+    helper.enableDbBinlog()
+    sql "DROP TABLE IF EXISTS ${dbName}.${tableName}"
+    target_sql "DROP TABLE IF EXISTS ${dbNameTarget}.${tableName}"
+    sql """
+        CREATE TABLE if NOT EXISTS ${tableName}
+        (
+            `test` INT,
+            `id` INT,
+            `value` INT
+        )
+        ENGINE=OLAP
+        UNIQUE KEY(`test`, `id`)
+        DISTRIBUTED BY HASH(test) BUCKETS 1
+        PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "binlog.enable" = "true"
+        )
+    """
+
+    helper.enableDbBinlog()
+    helper.ccrJobDelete()
+    helper.ccrJobCreate()
+
+    assertTrue(helper.checkRestoreFinishTimesOf("${tableName}", 30))
+
+    assertTrue(helper.checkShowTimesOf(""" SHOW TABLES LIKE "${tableName}" """, exist, 60, "target"))
+
+    // 1. Pause ccr job
+    helper.ccrJobPause()
+
+    // 2. Insert N data
+    for (int index = 0; index < insert_num; index++) {
+        sql """
+            INSERT INTO ${tableName} VALUES (${test_num}, ${index}, ${index})
+            """
+    }
+
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == insert_num }, 60, "sql"))
+
+    // 3. Do operation & wait it finishes upstream
+    sql """
+        ALTER TABLE ${tableName}
+        ADD COLUMN `first` INT KEY DEFAULT "1" FIRST
+        """
+    assertTrue(helper.checkShowTimesOf(""" show columns from ${tableName} """, { r -> return r[0][0] == 'first' }, 60, "sql"))
+
+    sql """
+        ALTER TABLE ${tableName}
+        MODIFY COLUMN `id` BIGINT KEY
+        """
+    assertTrue(helper.checkShowTimesOf("""
+                                SHOW ALTER TABLE COLUMN
+                                FROM ${dbName}
+                                WHERE TableName = "${tableName}" AND State = "FINISHED"
+                                """,
+                                has_count(1), 30))
+
+    assertTrue(helper.checkShowTimesOf("SHOW COLUMNS FROM `${tableName}`", id_is_big_int, 60, "sql"))
+    sql """
+        ALTER TABLE ${tableName}
+        MODIFY COLUMN `value` BIGINT
+        """
+    assertTrue(helper.checkShowTimesOf("""
+                                SHOW ALTER TABLE COLUMN
+                                FROM ${dbName}
+                                WHERE TableName = "${tableName}" AND State = "FINISHED"
+                                """,
+                                has_count(2), 30))
+
+    assertTrue(helper.checkShowTimesOf("SHOW COLUMNS FROM ${tableName}", value_is_big_int, 60, "sql"))
+    // 4. Insert N data
+    for (int index = insert_num; index < insert_num * 2; index++) {
+        sql """
+            INSERT INTO ${tableName} VALUES (${test_new_column_num}, ${test_num}, ${index}, ${index})
+            """
+    }
+
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == insert_num * 2 }, 60, "sql"))
+
+    // 5. Force trigger fullsnapshot
+    helper.force_fullsync()
+
+    // 6. Resume ccr job
+    helper.ccrJobResume()
+  
+    // 7. Verify data and operation are synced downstream
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == insert_num * 2 }, 60, "target"))
+    assertTrue(helper.checkShowTimesOf("SHOW COLUMNS FROM `${tableName}`", id_is_big_int, 60, "target"))
+    assertTrue(helper.checkShowTimesOf("SHOW COLUMNS FROM ${tableName}", value_is_big_int, 60, "target"))
+}
+
