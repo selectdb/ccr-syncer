8fea383bb38ccfeae79a9fbd337cb516315fefc9
koarz <lihao@selectdb.com>
2025-03-31 15:05:49 +0800

    fix: do normalize before create view (#563)

--- pkg/ccr/base/spec.go
+++ pkg/ccr/base/spec.go
@@ -616,15 +616,7 @@ func (s *Spec) CreateTableOrView(createTable *record.CreateTable, srcDatabase st
 	//	When create view, the db name of sql is source db name, we should use dest db name to create view
 	createSql := createTable.Sql
 	if createTable.IsCreateView() {
-		log.Tracef("create view, use dest db name to replace source db name")
-
-		// replace `internal`.`source_db_name`. or `default_cluster:source_db_name`. to `internal`.`dest_db_name`.
-		originalNameNewStyle := "`internal`.`" + strings.TrimSpace(srcDatabase) + "`."
-		originalNameOldStyle := "`default_cluster:" + strings.TrimSpace(srcDatabase) + "`." // for Doris 2.0.x
-		replaceName := "`internal`.`" + strings.TrimSpace(s.Database) + "`."
-		createSql = strings.ReplaceAll(
-			strings.ReplaceAll(createSql, originalNameNewStyle, replaceName), originalNameOldStyle, replaceName)
-		log.Debugf("original create view sql is %s, after replace, now sql is %s", createTable.Sql, createSql)
+		createSql = NormalizeCreateViewSql(s.Database, srcDatabase, createSql)
 	}
 
 	createSql = AddDBPrefixToCreateTableOrViewSql(s.Database, createSql)
@@ -1648,4 +1640,21 @@ func HandleSchemaChangeDefaultValue(sql string, lightningSchemaChange *record.Mo
 		}
 	}
 	return sql
+}
+
+func NormalizeCreateViewSql(destDatabase string, srcDatabase string, createSql string) string {
+	log.Tracef("create view, use dest db name to replace source db name")
+	originSql := createSql
+	srcDatabase = strings.TrimSpace(srcDatabase)
+	// the createSql may not contain `internal`. prefix
+	createSql = strings.ReplaceAll(createSql, " `"+srcDatabase+"`.", " `internal`.`"+srcDatabase+"`.")
+
+	// replace `internal`.`source_db_name`. or `default_cluster:source_db_name`. to `internal`.`dest_db_name`.
+	originalNameNewStyle := "`internal`.`" + srcDatabase + "`."
+	originalNameOldStyle := "`default_cluster:" + srcDatabase + "`." // for Doris 2.0.x
+	replaceName := "`internal`.`" + strings.TrimSpace(destDatabase) + "`."
+	createSql = strings.ReplaceAll(
+		strings.ReplaceAll(createSql, originalNameNewStyle, replaceName), originalNameOldStyle, replaceName)
+	log.Debugf("original create view sql is %s, after replace, now sql is %s", originSql, createSql)
+	return createSql
 }
