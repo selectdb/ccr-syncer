adc1aa5ca098da7664bc0aff35370909a3e681d0
koarz <lihao@selectdb.com>
2025-04-02 11:20:36 +0800

    move

--- regression-test/suites/db_sync/alt_prop/sequence/test_ds_alt_prop_seq.groovy
+++ regression-test/suites/db_sync/alt_prop/sequence/test_ds_alt_prop_seq.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_ds_alt_prop_seq_col") {
+suite("test_ds_alt_prop_seq") {
     def helper = new GroovyShell(new Binding(['suite': delegate]))
             .evaluate(new File("${context.config.suitePath}/../common", "helper.groovy"))
 
