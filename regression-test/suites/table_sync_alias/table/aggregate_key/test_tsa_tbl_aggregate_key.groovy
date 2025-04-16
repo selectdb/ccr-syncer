e59b4f10735b08454f6419d7226d9182e47e2faa
wyxxxcat <wangyixuan@selectdb.com>
2025-04-16 10:35:25 +0800

    1

--- regression-test/suites/table_sync_alias/table/aggregate_key/test_tsa_tbl_aggregate_key.groovy
+++ regression-test/suites/table_sync_alias/table/aggregate_key/test_tsa_tbl_aggregate_key.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_tsa_tbl_unique") {
+suite("test_tsa_tbl_aggregate_key") {
     def helper = new GroovyShell(new Binding(['suite': delegate]))
             .evaluate(new File("${context.config.suitePath}/../common", "helper.groovy"))
 
@@ -32,26 +32,21 @@ suite("test_tsa_tbl_unique") {
     }
 
     sql "DROP TABLE IF EXISTS ${dbName}.${tableName}"
-    target_sql "DROP TABLE IF EXISTS TEST_${dbName}.${tableName}"
+    target_sql "DROP TABLE IF EXISTS TEST_${dbName}.${aliasName}"
 
     helper.enableDbBinlog()
 
     sql """
-        CREATE TABLE if NOT EXISTS ${tableName}
-        (
-            `test` INT,
-            `id` INT
+        CREATE TABLE ${tableName}(
+            `id` int,
+            `age` int REPLACE_IF_NOT_NULL NULL
         )
-        ENGINE=OLAP
-        UNIQUE KEY(`test`, `id`)
-        PARTITION BY RANGE(`id`)
-        (
-        )
-        DISTRIBUTED BY HASH(id) BUCKETS 1
-        PROPERTIES (
+        AGGREGATE KEY(`id`)
+        DISTRIBUTED BY HASH(id) BUCKETS 3 
+        PROPERTIES ( 
             "replication_allocation" = "tag.location.default: 1",
             "binlog.enable" = "true"
-        )
+        );
     """
 
     helper.ccrJobDelete(tableName)
@@ -63,9 +58,19 @@ suite("test_tsa_tbl_unique") {
 
     assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${aliasName}\"", exist, 60, "target"))
 
-    def sql_res = sql "SHOW CREATE TABLE ${tableName}"
+    sql "INSERT INTO ${tableName} VALUES (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)"
 
-    def target_res = target_sql "SHOW CREATE TABLE ${aliasName}"
+    assertTrue(helper.checkSelectTimesOf(""" select * from ${aliasName} """, 5, 30))
 
-    assertTrue(target_res[0][1].contains("UNIQUE KEY(`test`, `id`)"))
+    assertTrue(helper.checkShowTimesOf(" select age from ${aliasName} where id = 3 ", { r -> r[0][0] == 3}, 60, "target"))
+
+    sql "INSERT INTO ${tableName} VALUES (3, 5) "
+
+    assertTrue(helper.checkShowTimesOf(" select age from ${aliasName} where id = 3 ", { r -> r[0][0] == 5}, 60, "target"))
+
+    sql "INSERT INTO ${tableName} VALUES (2, 4), (5, 7) "
+
+    assertTrue(helper.checkShowTimesOf(" select age from ${aliasName} where id = 2 ", { r -> r[0][0] == 4}, 60, "target"))
+
+    assertTrue(helper.checkShowTimesOf(" select age from ${aliasName} where id = 5 ", { r -> r[0][0] == 7}, 60, "target"))
 }
