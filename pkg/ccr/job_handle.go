1b3cd33fd4e71302a64527286485d92f911e6bad
koarz <lihao@selectdb.com>
2025-04-18 11:11:47 +0800

    fix: fix handle import wrong log (#598)

--- pkg/ccr/job_handle.go
+++ pkg/ccr/job_handle.go
@@ -3,10 +3,10 @@ package ccr
 import (
 	"reflect"
 
-	"github.com/cloudwego/kitex/tool/internal_pkg/log"
 	"github.com/selectdb/ccr_syncer/pkg/ccr/record"
 	festruct "github.com/selectdb/ccr_syncer/pkg/rpc/kitex_gen/frontendservice"
 	"github.com/selectdb/ccr_syncer/pkg/xerror"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
