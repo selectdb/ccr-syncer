5c5687c1442506f519e56f00118c9d1c0cbe6bbc
koarz <lihao@selectdb.com>
2025-04-22 18:35:10 +0800

    1

--- regression-test/suites/db_sync/partition/default/test_ds_partition_default_list_insert.groovy
+++ regression-test/suites/db_sync/partition/default/test_ds_partition_default_list_insert.groovy
@@ -30,9 +30,14 @@ suite("test_ds_partition_default_list_insert") {
         return res.size() != 0
     }
 
+    def notExist = { res -> Boolean
+        return res.size() == 0
+    }
+
     helper.ccrJobDelete()
     helper.ccrJobCreate()
 
+    logger.info("=== insert into fullsync table with default partition ===")
     sql """
         CREATE TABLE IF NOT EXISTS ${tableName} (
             id INT,
@@ -48,6 +53,68 @@ suite("test_ds_partition_default_list_insert") {
         )
         """
     assertTrue(helper.checkRestoreFinishTimesOf("${tableName}", 30))
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 30, "sql"))
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 30, "target"))
+
+    sql "insert into ${tableName} values (1, 1)"
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == 1}, 60, "target"))
+    sql "insert into ${tableName} values (2, 2)"
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == 2}, 60, "target"))
+
+    sql "DROP TABLE IF EXISTS ${dbName}.${tableName}"
+    target_sql "DROP TABLE IF EXISTS TEST_${dbName}.${tableName}"
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", notExist, 30, "sql"))
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", notExist, 30, "target"))
+
+    logger.info("=== insert into increment table with default partition ===")
+    sql """
+        CREATE TABLE IF NOT EXISTS ${tableName} (
+            id INT,
+            k1 INT
+        )
+        DUPLICATE KEY(id)
+        PARTITION BY LIST (id)
+        (PARTITION p1)
+        DISTRIBUTED BY HASH(id) BUCKETS 10
+        PROPERTIES(
+            "replication_allocation" = "tag.location.default: 1",
+            "binlog.enable" = "true"
+        )
+        """
+
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 30, "sql"))
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 30, "target"))
+
+    sql "insert into ${tableName} values (1, 1)"
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == 1}, 60, "target"))
+    sql "insert into ${tableName} values (2, 2)"
+    assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == 2}, 60, "target"))
+
+    sql "DROP TABLE IF EXISTS ${dbName}.${tableName}"
+    target_sql "DROP TABLE IF EXISTS TEST_${dbName}.${tableName}"
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", notExist, 30, "sql"))
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", notExist, 30, "target"))
+
+    logger.info("=== insert into increment table with alter add default partition ===")
+    sql """
+        CREATE TABLE IF NOT EXISTS ${tableName} (
+            id INT,
+            k1 INT
+        )
+        DUPLICATE KEY(id)
+        PARTITION BY LIST (id)
+        (PARTITION p1 values in("1"))
+        DISTRIBUTED BY HASH(id) BUCKETS 10
+        PROPERTIES(
+            "replication_allocation" = "tag.location.default: 1",
+            "binlog.enable" = "true"
+        )
+        """
+
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 30, "sql"))
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 30, "target"))
+
+    sql "ALTER TABLE ${tableName} ADD PARTITION `p2`;"
 
     sql "insert into ${tableName} values (1, 1)"
     assertTrue(helper.checkShowTimesOf(""" select * from ${tableName} """, { r -> r.size() == 1}, 60, "target"))
