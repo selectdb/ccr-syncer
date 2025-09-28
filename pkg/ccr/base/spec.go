selectdb/ccr-syncer

commit fe86fa50c8f9987e5bb39dfff39651b2bf0a438a

feat: support modify distribution bucket num binlog (#581)

pkg/ccr/base/spec.go
@@ -1466,6 +1466,23 @@ func (s *Spec) DropRollup(destTableName, rollup string) error {
 	return s.Exec(dropRollupSql)
 }
 
+func (s *Spec) ModifyDistributionBucketNum(destTableName string, bucketType string, autoBucket bool, bucketNum int, columnsName string) error {
+	dbName := utils.FormatKeywordName(s.Database)
+	destTableName = utils.FormatKeywordName(destTableName)
+	modifyDistributionBucketNumSql := "ALTER TABLE " + dbName + "." + destTableName + " MODIFY DISTRIBUTION DISTRIBUTED BY " + bucketType
+	if bucketType == "HASH" {
+		modifyDistributionBucketNumSql += fmt.Sprintf("(%s)", columnsName)
+	}
+	modifyDistributionBucketNumSql += " BUCKETS "
+	if autoBucket {
+		modifyDistributionBucketNumSql += "AUTO"
+	} else {
+		modifyDistributionBucketNumSql += fmt.Sprintf("%d", bucketNum)
+	}
+	log.Infof("modify distribution bucket num sql: %s", modifyDistributionBucketNumSql)
+	return s.Exec(modifyDistributionBucketNumSql)
+}
+
 func (s *Spec) SyncTables(tables ...string) error {
 	var err error
 

pkg/ccr/base/specer.go
@@ -76,5 +76,7 @@ type Specer interface {
 	DesyncTables(tables ...string) error
 	SyncTables(tables ...string) error
 
+	ModifyDistributionBucketNum(destTableName string, bucketType string, autoBucket bool, bucketNum int, columnsName string) error
+
 	utils.Subject[SpecEvent]
 }

pkg/ccr/job.go
@@ -3277,6 +3277,7 @@ func (j *Job) determineBinlogState(binlog *festruct.TBinlog) (bool, error) {
 		case festruct.TBinlogType_MODIFY_TABLE_PROPERTY:
 		case festruct.TBinlogType_MODIFY_PARTITIONS:
 		case festruct.TBinlogType_INDEX_CHANGE_JOB:
+		case festruct.TBinlogType_MODIFY_DISTRIBUTION_BUCKET_NUM:
 
 		default:
 			return false

pkg/ccr/handle/modify_distribution_bucket_num.go
@@ -1,0 +1,34 @@
+package handle
+
+import (
+	"github.com/selectdb/ccr_syncer/pkg/ccr"
+	"github.com/selectdb/ccr_syncer/pkg/ccr/record"
+	festruct "github.com/selectdb/ccr_syncer/pkg/rpc/kitex_gen/frontendservice"
+)
+
+func init() {
+	ccr.RegisterJobHandle[*record.ModifyDistributionBucketNum](festruct.TBinlogType_MODIFY_DISTRIBUTION_BUCKET_NUM, &ModifyDistributionBucketNumHandle{})
+}
+
+type ModifyDistributionBucketNumHandle struct {
+	// The modify distribution bucket num binlog is idempotent
+	IdempotentJobHandle[*record.ModifyDistributionBucketNum]
+}
+
+func (h *ModifyDistributionBucketNumHandle) Handle(j *ccr.Job, commitSeq int64, modifyDistributionBucketNum *record.ModifyDistributionBucketNum) error {
+	var destTableName string
+	if j.SyncType == ccr.TableSync {
+		destTableName = j.Dest.Table
+	} else {
+		table, err := j.GetDestMeta().GetTable(modifyDistributionBucketNum.TableId)
+		if err != nil {
+			return err
+		}
+		destTableName = table.Name
+	}
+	bucketType := modifyDistributionBucketNum.Type
+	autoBucket := modifyDistributionBucketNum.AutoBucket
+	bucketNum := modifyDistributionBucketNum.BucketNum
+	columnsName := modifyDistributionBucketNum.ColumnsName
+	return j.IDest.ModifyDistributionBucketNum(destTableName, bucketType, autoBucket, bucketNum, columnsName)
+}

pkg/ccr/record/modify_distribution_bucket_num.go
@@ -1,0 +1,71 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License
+package record
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/selectdb/ccr_syncer/pkg/xerror"
+)
+
+type ModifyDistributionBucketNum struct {
+	DbId        int64  `json:"dbId"`
+	TableId     int64  `json:"tableId"`
+	Type        string `json:"Type"`
+	AutoBucket  bool   `json:"autoBucket"`
+	BucketNum   int    `json:"bucketNum"`
+	ColumnsName string `json:"columnsName"`
+}
+
+func (modifyDistributionBucketNum *ModifyDistributionBucketNum) Deserialize(data string) error {
+	err := json.Unmarshal([]byte(data), &modifyDistributionBucketNum)
+	if err != nil {
+		return xerror.Wrap(err, xerror.Normal, "unmarshal modify distribution bucket num error")
+	}
+
+	if modifyDistributionBucketNum.Type != "RANDOM" && modifyDistributionBucketNum.Type != "HASH" {
+		return xerror.Errorf(xerror.Normal, "distribution type can not recognize")
+	}
+
+	if modifyDistributionBucketNum.TableId == 0 {
+		return xerror.Errorf(xerror.Normal, "table id not found")
+	}
+
+	if modifyDistributionBucketNum.BucketNum <= 0 {
+		return xerror.Errorf(xerror.Normal, "bucket num is invalid")
+	}
+	return nil
+}
+
+func NewModifyDistributionBucketNumFromJson(data string) (*ModifyDistributionBucketNum, error) {
+	var modifyDistributionBucketNum ModifyDistributionBucketNum
+	if err := modifyDistributionBucketNum.Deserialize(data); err != nil {
+		return nil, err
+	}
+	return &modifyDistributionBucketNum, nil
+}
+
+// String
+func (c *ModifyDistributionBucketNum) String() string {
+	return fmt.Sprintf("ModifyDistributionBucketNum: DbId: %d, TableId: %d, Type: %s, AutoBucket: %v, BucketNum: %d, ColumnsName: %s",
+		c.DbId, c.TableId, c.Type, c.AutoBucket, c.BucketNum, c.ColumnsName)
+}
+
+func (record *ModifyDistributionBucketNum) GetTableId() int64 {
+	return record.TableId
+}

pkg/rpc/kitex_gen/frontendservice/FrontendService.go
@@ -477,8 +477,8 @@ const (
 	TBinlogType_RENAME_PARTITION                          TBinlogType = 22
 	TBinlogType_DROP_ROLLUP                               TBinlogType = 23
 	TBinlogType_RECOVER_INFO                              TBinlogType = 24
-	TBinlogType_MIN_UNKNOWN                               TBinlogType = 25
-	TBinlogType_UNKNOWN_10                                TBinlogType = 26
+	TBinlogType_MODIFY_DISTRIBUTION_BUCKET_NUM            TBinlogType = 25
+	TBinlogType_MIN_UNKNOWN                               TBinlogType = 26
 	TBinlogType_UNKNOWN_11                                TBinlogType = 27
 	TBinlogType_UNKNOWN_12                                TBinlogType = 28
 	TBinlogType_UNKNOWN_13                                TBinlogType = 29
@@ -623,10 +623,10 @@ func (p TBinlogType) String() string {
 		return "DROP_ROLLUP"
 	case TBinlogType_RECOVER_INFO:
 		return "RECOVER_INFO"
+	case TBinlogType_MODIFY_DISTRIBUTION_BUCKET_NUM:
+		return "MODIFY_DISTRIBUTION_BUCKET_NUM"
 	case TBinlogType_MIN_UNKNOWN:
 		return "MIN_UNKNOWN"
-	case TBinlogType_UNKNOWN_10:
-		return "UNKNOWN_10"
 	case TBinlogType_UNKNOWN_11:
 		return "UNKNOWN_11"
 	case TBinlogType_UNKNOWN_12:
@@ -863,10 +863,10 @@ func TBinlogTypeFromString(s string) (TBinlogType, error) {
 		return TBinlogType_DROP_ROLLUP, nil
 	case "RECOVER_INFO":
 		return TBinlogType_RECOVER_INFO, nil
+	case "MODIFY_DISTRIBUTION_BUCKET_NUM":
+		return TBinlogType_MODIFY_DISTRIBUTION_BUCKET_NUM, nil
 	case "MIN_UNKNOWN":
 		return TBinlogType_MIN_UNKNOWN, nil
-	case "UNKNOWN_10":
-		return TBinlogType_UNKNOWN_10, nil
 	case "UNKNOWN_11":
 		return TBinlogType_UNKNOWN_11, nil
 	case "UNKNOWN_12":

pkg/rpc/thrift/FrontendService.thrift
@@ -1028,6 +1028,7 @@ enum TBinlogType {
   RENAME_PARTITION = 22,
   DROP_ROLLUP = 23,
   RECOVER_INFO = 24,
+  MODIFY_DISTRIBUTION_BUCKET_NUM = 25
 
   // Keep some IDs for allocation so that when new binlog types are added in the
   // future, the changes can be picked back to the old versions without breaking
@@ -1044,8 +1045,7 @@ enum TBinlogType {
   //    MODIFY_XXX = 17,
   //    MIN_UNKNOWN = 18,
   //    UNKNOWN_3 = 19,
-  MIN_UNKNOWN = 25,
-  UNKNOWN_10 = 26,
+  MIN_UNKNOWN = 26,
   UNKNOWN_11 = 27,
   UNKNOWN_12 = 28,
   UNKNOWN_13 = 29,

regression-test/suites/db_sync/alt_prop/bucket/test_ds_alt_prop_bucket.groovy
@@ -82,5 +82,5 @@ suite("test_ds_alt_prop_bucket") {
 
     assertTrue(helper.checkShowTimesOf("SHOW CREATE TABLE ${tableName}", existNewBucket, 60, "sql"))
 
-    assertTrue(helper.checkShowTimesOf("SHOW CREATE TABLE ${tableName}", existOldBucket, 60, "target"))
+    assertTrue(helper.checkShowTimesOf("SHOW CREATE TABLE ${tableName}", existNewBucket, 60, "target"))
 }

commit 261c0f276fb03a41969f2b8a706b86e070f2ded3

fix some issues
Introduced by #581, #596

1. handle MODIFY_DISTRIBUTION_BUCKET_NUM/_TYPE correctly
2. Call Next() before scan CreateTableSql

pkg/ccr/base/spec.go
@@ -1559,6 +1559,43 @@ func (s *Spec) ModifyTableProperty(destTableName string, modifyProperty *record.
 	return s.Exec(sql)
 }
 
+func (s *Spec) GetCreateTableSql(tableName string) (string, error) {
+	dbName := utils.FormatKeywordName(s.Database)
+	tableName = utils.FormatKeywordName(tableName)
+	query := fmt.Sprintf("SHOW CREATE TABLE %s.%s", dbName, tableName)
+	log.Infof("show create table sql: %s", query)
+
+	db, err := s.Connect()
+	if err != nil {
+		return "", xerror.Wrapf(err, xerror.Normal, "show create table sql: connect to %s", s.String())
+	}
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return "", xerror.Wrapf(err, xerror.Normal, "show create table %s", tableName)
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		return "", xerror.Errorf(xerror.Normal, "show create table %s: result is empty", tableName)
+	}
+
+	rowParser := utils.NewRowParser()
+	if err := rowParser.Parse(rows); err != nil {
+		return "", xerror.Wrapf(err, xerror.Normal, "parse show create table %s rows", tableName)
+	}
+	createSql, err := rowParser.GetString("Create Table")
+	if err != nil {
+		return "", xerror.Wrapf(err, xerror.Normal, "parse show create table %s fields", tableName)
+	}
+
+	if err := rows.Err(); err != nil {
+		return "", xerror.Wrapf(err, xerror.Normal, "show create table %s rows error", tableName)
+	}
+
+	return createSql, nil
+}
+
 // Determine whether the error are network related, eg connection refused, connection reset, exposed from net packages.
 func isNetworkRelated(err error) bool {
 	msg := err.Error()

pkg/ccr/base/specer.go
@@ -79,5 +79,7 @@ type Specer interface {
 	ModifyDistributionType(destTableName string) error
 	ModifyDistributionBucketNum(destTableName string, bucketType string, autoBucket bool, bucketNum int, columnsName string) error
 
+	GetCreateTableSql(tableName string) (string, error)
+
 	utils.Subject[SpecEvent]
 }

pkg/ccr/job.go
@@ -2988,6 +2988,55 @@ func (j *Job) handleRecoverInfo(binlog *festruct.TBinlog) error {
 	return j.NewPartialSnapshot(recoverInfo.TableId, recoverInfo.TableName, nil, true, isView)
 }
 
+func (j *Job) handleModifyDistributionBucketNum(binlog *festruct.TBinlog) error {
+	log.Infof("handle modify distribution bucket num binlog, prevCommitSeq: %d, commitSeq: %d",
+		j.progress.PrevCommitSeq, j.progress.CommitSeq)
+
+	data := binlog.GetData()
+	modifyDistributionBucketNum, err := record.NewModifyDistributionBucketNumFromJson(data)
+	if err != nil {
+		return err
+	}
+
+	commitSeq := binlog.GetCommitSeq()
+	if j.IsBinlogCommitted(modifyDistributionBucketNum.TableId, commitSeq) {
+		return nil
+	}
+
+	destTableName, err := j.GetDestNameBySrcId(modifyDistributionBucketNum.TableId)
+	if err != nil {
+		return err
+	}
+	bucketType := modifyDistributionBucketNum.Type
+	autoBucket := modifyDistributionBucketNum.AutoBucket
+	bucketNum := modifyDistributionBucketNum.BucketNum
+	columnsName := modifyDistributionBucketNum.ColumnsName
+	return j.IDest.ModifyDistributionBucketNum(destTableName, bucketType, autoBucket, bucketNum, columnsName)
+}
+
+func (j *Job) handleModifyDistributionType(binlog *festruct.TBinlog) error {
+	log.Infof("handle modify distribution type binlog, prevCommitSeq: %d, commitSeq: %d",
+		j.progress.PrevCommitSeq, j.progress.CommitSeq)
+
+	data := binlog.GetData()
+	modifyDistributionType, err := record.NewModifyDistributionTypeFromJson(data)
+	if err != nil {
+		return err
+	}
+
+	commitSeq := binlog.GetCommitSeq()
+	if j.IsBinlogCommitted(modifyDistributionType.TableId, commitSeq) {
+		return nil
+	}
+
+	destTableName, err := j.GetDestNameBySrcId(modifyDistributionType.TableId)
+	if err != nil {
+		return err
+	}
+
+	return j.IDest.ModifyDistributionType(destTableName)
+}
+
 func (j *Job) handleBarrier(binlog *festruct.TBinlog) error {
 	data := binlog.GetData()
 	barrierLog, err := record.NewBarrierLogFromJson(data)
@@ -3556,6 +3605,10 @@ func (j *Job) handleNonBarrierBinlog(binlog *festruct.TBinlog) error {
 			err = j.handleDropRollup(binlog, false)
 		case festruct.TBinlogType_RECOVER_INFO:
 			err = j.handleRecoverInfo(binlog)
+		case festruct.TBinlogType_MODIFY_DISTRIBUTION_BUCKET_NUM:
+			err = j.handleModifyDistributionBucketNum(binlog)
+		case festruct.TBinlogType_MODIFY_DISTRIBUTION_TYPE:
+			err = j.handleModifyDistributionType(binlog)
 		default:
 			return xerror.Errorf(xerror.Normal, "unknown binlog type: %v, commit seq %d, data %s",
 				binlogType, commitSeq, binlog.GetData())
@@ -4055,28 +4108,11 @@ func (j *Job) Desync() error {
 
 // check show create table contain some string
 func (j *Job) CheckCreateTable(tableName, expectedStr string) (bool, error) {
-	db, err := j.Dest.Connect()
+	createSql, err := j.IDest.GetCreateTableSql(tableName)
 	if err != nil {
-		return false, err
+		return false, xerror.Wrapf(err, xerror.Normal, "check create table %s", tableName)
 	}
 
-	dbName := utils.FormatKeywordName(j.Dest.Database)
-	tableName = utils.FormatKeywordName(tableName)
-	query := fmt.Sprintf("SHOW CREATE TABLE %s.%s", dbName, tableName)
-	log.Infof("show create table sql: %s", query)
-	rows, err := db.Query(query)
-	if err != nil {
-		return false, xerror.Wrapf(err, xerror.Normal, "show create table %s", tableName)
-	}
-	defer rows.Close()
-	rowParser := utils.NewRowParser()
-	if err := rowParser.Parse(rows); err != nil {
-		return false, xerror.Wrapf(err, xerror.Normal, "parse show create table %s rows", tableName)
-	}
-	createSql, err := rowParser.GetString("Create Table")
-	if err != nil {
-		return false, xerror.Wrapf(err, xerror.Normal, query)
-	}
 	return strings.Contains(createSql, expectedStr), nil
 }
 