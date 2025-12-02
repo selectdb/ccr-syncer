3b64819dd4e41b900d4e50112ccb1e1640c42e6c
walter <w41ter.l@gmail.com>
2025-12-02 10:17:38 +0800

    chore: add drop key column in parallel test (#657)

--- regression-test/suites/table_sync_alias_dep/column/drop_key/test_tsad_column_drop_key.groovy
+++ regression-test/suites/table_sync_alias_dep/column/drop_key/test_tsad_column_drop_key.groovy
@@ -43,11 +43,11 @@ suite("test_tsad_column_drop_key") {
         CREATE TABLE if NOT EXISTS ${tableName}
         (
             `test` INT,
-            `value` INT,
-            `id` INT
+            `id` INT,
+            `value` INT
         )
         ENGINE=OLAP
-        DUPLICATE KEY(`test`, `value`)
+        DUPLICATE KEY(`test`, `id`)
         DISTRIBUTED BY HASH(`value`) BUCKETS 1
         PROPERTIES (
             "replication_allocation" = "tag.location.default: 1",
